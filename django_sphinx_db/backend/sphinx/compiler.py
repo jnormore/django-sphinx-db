bbbbbb95dc94eafe78d965c7fb1a325e2915a2f0
Jason Normore <jnormore@gmail.com>
2013-03-03 02:02:26 -0500

    Adds setting for RT index and updates sql update compiler to not use replace into for non RT

--- django_sphinx_db/backend/sphinx/compiler.py
+++ django_sphinx_db/backend/sphinx/compiler.py
@@ -2,6 +2,7 @@ from django.db.models.sql import compiler
 from django.db.models.sql.where import WhereNode
 from django.db.models.sql.where import EmptyShortCircuit, EmptyResultSet
 from django.db.models.sql.expressions import SQLEvaluator
+from django.conf import settings
 
 
 class SphinxWhereNode(WhereNode):
@@ -80,44 +81,47 @@ class SQLDeleteCompiler(compiler.SQLDeleteCompiler, SphinxQLCompiler):
 
 class SQLUpdateCompiler(compiler.SQLUpdateCompiler, SphinxQLCompiler):
     def as_sql(self):
-        qn = self.connection.ops.quote_name
-        opts = self.query.model._meta
-        result = ['REPLACE INTO %s' % qn(opts.db_table)]
-        # This is a bit ugly, we have to scrape information from the where clause
-        # and put it into the field/values list. Sphinx will not accept an UPDATE
-        # statement that includes full text data, only INSERT/REPLACE INTO.
-        lvalue, lookup_type, value_annot, params_or_value = self.query.where.children[0].children[0]
-        (table_name, column_name, column_type), val = lvalue.process(lookup_type, params_or_value, self.connection)
-        fields, values, params = [column_name], ['%s'], [val[0]]
-        # Now build the rest of the fields into our query.
-        for field, model, val in self.query.values:
-            if hasattr(val, 'prepare_database_save'):
-                val = val.prepare_database_save(field)
-            else:
-                val = field.get_db_prep_save(val, connection=self.connection)
+        if getattr(settings, 'SPHINX_INDEX_RT', True):
+            qn = self.connection.ops.quote_name
+            opts = self.query.model._meta
+            result = ['REPLACE INTO %s' % qn(opts.db_table)]
+            # This is a bit ugly, we have to scrape information from the where clause
+            # and put it into the field/values list. Sphinx will not accept an UPDATE
+            # statement that includes full text data, only INSERT/REPLACE INTO.
+            lvalue, lookup_type, value_annot, params_or_value = self.query.where.children[0].children[0]
+            (table_name, column_name, column_type), val = lvalue.process(lookup_type, params_or_value, self.connection)
+            fields, values, params = [column_name], ['%s'], [val[0]]
+            # Now build the rest of the fields into our query.
+            for field, model, val in self.query.values:
+                if hasattr(val, 'prepare_database_save'):
+                    val = val.prepare_database_save(field)
+                else:
+                    val = field.get_db_prep_save(val, connection=self.connection)
 
-            # Getting the placeholder for the field.
-            if hasattr(field, 'get_placeholder'):
-                placeholder = field.get_placeholder(val, self.connection)
-            else:
-                placeholder = '%s'
+                # Getting the placeholder for the field.
+                if hasattr(field, 'get_placeholder'):
+                    placeholder = field.get_placeholder(val, self.connection)
+                else:
+                    placeholder = '%s'
 
-            if hasattr(val, 'evaluate'):
-                val = SQLEvaluator(val, self.query, allow_joins=False)
-            name = field.column
-            if hasattr(val, 'as_sql'):
-                sql, params = val.as_sql(qn, self.connection)
-                values.append(sql)
-                params.extend(params)
-            elif val is not None:
-                values.append(placeholder)
-                params.append(val)
-            else:
-                values.append('NULL')
-            fields.append(name)
-        result.append('(%s)' % ', '.join(fields))
-        result.append('VALUES (%s)' % ', '.join(values))
-        return ' '.join(result), params
+                if hasattr(val, 'evaluate'):
+                    val = SQLEvaluator(val, self.query, allow_joins=False)
+                name = field.column
+                if hasattr(val, 'as_sql'):
+                    sql, params = val.as_sql(qn, self.connection)
+                    values.append(sql)
+                    params.extend(params)
+                elif val is not None:
+                    values.append(placeholder)
+                    params.append(val)
+                else:
+                    values.append('NULL')
+                fields.append(name)
+            result.append('(%s)' % ', '.join(fields))
+            result.append('VALUES (%s)' % ', '.join(values))
+            return ' '.join(result), params
+        else:
+            return super(SQLUpdateCompiler, self).as_sql()
 
 
 class SQLAggregateCompiler(compiler.SQLAggregateCompiler, SphinxQLCompiler):
